26d16b631532a5b61541fa8c005de6cc13d12a7a
ni3_per <nitinjaniyani7@gmail.com>
2021-03-21 01:02:36 +0530

    add chagnes

--- gulpfile.js
+++ gulpfile.js
@@ -27,81 +27,82 @@ function errorLog(error) {
 // --------------------------------------------------
 
 // Sass - Compile Sass files into CSS
-gulp.task('sass', function () {
-	gulp.src('HTML/sass/**/*.scss')
-		.pipe(changed('HTML/css/'))
+gulp.task('sass',  () => {
+return	gulp.src('./HTML/sass/**/*.scss')
+		.pipe(changed('./HTML/css/'))
 		.pipe(sass({ outputStyle: 'expanded' }))
 		.on('error', sass.logError)
-		.pipe(gulp.dest('HTML/css/'));
+		.pipe(gulp.dest('./HTML/css/'));
 });
 
 
 // Minify CSS
-gulp.task('minify-css', function() {
+gulp.task('minify-css',  gulp.series(() => {
 	// Theme
-    gulp.src(['HTML/css/layout.css', '!HTML/css/layout.min.css'])
-        .pipe(cleanCSS({debug: true}, function(details) {
+  return  gulp.src(['./HTML/css/layout.css', '!./HTML/css/layout.min.css'])
+        .pipe(cleanCSS({debug: true}, (details) => {
             console.log(details.name + ': ' + details.stats.originalSize);
             console.log(details.name + ': ' + details.stats.minifiedSize);
         }))
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('HTML/css/'));
+        .pipe(gulp.dest('./HTML/css/'));
 
     // RTL
-    gulp.src(['HTML/css/layout-rtl.css', '!HTML/css/layout-rtl.min.css'])
-        .pipe(cleanCSS({debug: true}, function(details) {
+    gulp.src(['./HTML/css/layout-rtl.css', '!./HTML/css/layout-rtl.min.css'])
+        .pipe(cleanCSS({debug: true}, (details) => {
             console.log(details.name + ': ' + details.stats.originalSize);
             console.log(details.name + ': ' + details.stats.minifiedSize);
         }))
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('HTML/css/'));
-});
+        .pipe(gulp.dest('./HTML/css/'));
+}));
 
 
 // RTL CSS - Convert LTR CSS to RTL.
-gulp.task('rtlcss', function () {
-	gulp.src(['HTML/css/layout.css', '!HTML/css/layout.min.css', '!HTML/css/layout-rtl.css', '!HTML/css/layout-rtl.min.css'])
-	.pipe(changed('HTML/css/'))
+gulp.task('rtlcss',  gulp.series( () => {
+	return	gulp.src(['./HTML/css/layout.css', '!./HTML/css/layout.min.css', '!./HTML/css/layout-rtl.css', '!./HTML/css/layout-rtl.min.css'])
+	.pipe(changed('./HTML/css/'))
 		.pipe(rtlcss())
 		.pipe(rename({ suffix: '-rtl' }))
-		.pipe(gulp.dest('HTML/css/'));
-});
+		.pipe(gulp.dest('./HTML/css/'));
+}));
 
 
 // Minify JS - Minifies JS
-gulp.task('uglify', function (cb) {
-  	pump([
-	        gulp.src(['HTML/js/**/*.js', '!HTML/js/**/*.min.js']),
+gulp.task('uglify',  gulp.series((cb) => {
+  return	pump([
+	        gulp.src(['./HTML/js/**/*.js', '!./HTML/js/**/*.min.js']),
 	        uglify(),
 			rename({ suffix: '.min' }),
-	        gulp.dest('HTML/js/')
+	        gulp.dest('./HTML/js/')
 		],
 		cb
 	);
-});
+}));
 
 
 // Htmlhint - Validate HTML
-gulp.task('htmlhint', function() {
-	gulp.src('HTML/*.html')
+gulp.task('htmlhint', gulp.series( () => {
+	return	gulp.src('./HTML/*.html')
 		.pipe(htmlhint())
 		.pipe(htmlhint.reporter())
 	  	.pipe(htmlhint.failReporter({ suppress: true }))
-});
+}));
 
 
 // --------------------------------------------------
 // [Gulp Task - Watch]
 // --------------------------------------------------
 
-// Lets us type "gulp" on the command line and run all of our tasks
-gulp.task('default', ['sass', 'minify-css', 'rtlcss', 'uglify', 'htmlhint', 'watch']);
-
 // This handles watching and running tasks
-gulp.task('watch', function () {
-    gulp.watch('HTML/sass/**/*.scss', ['sass']);
-    gulp.watch('HTML/css/layout.css', ['minify-css']);
-    gulp.watch('HTML/css/layout.css', ['rtlcss']);
-    gulp.watch('HTML/js/**/*.js', ['uglify']);
-    gulp.watch('HTML/*.html', ['htmlhint']);
-});
+gulp.task('watch',  gulp.series(() => {
+	gulp.watch('./HTML/sass/**/*.scss', gulp.series('sass'));
+	gulp.watch('./HTML/css/layout.css', gulp.series('minify-css'));
+	gulp.watch('./HTML/css/layout.css', gulp.series('rtlcss'));
+	gulp.watch('./HTML/js/**/*.js', gulp.series('uglify'));
+	gulp.watch('./HTML/*.html', gulp.series('htmlhint'));
+}));
+
+// Lets us type "gulp" on the command line and run all of our tasks
+gulp.task('default',  gulp.series('sass', 'minify-css', 'rtlcss', 'uglify', 'htmlhint', 'watch'));
+
